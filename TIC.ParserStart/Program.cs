eac5650b1b31bbe8c8284de621c2a612392ccb34
Jacob Kirkwood <jacob.kirkwood@bbconsult.co.uk>
2018-08-03 11:48:11 +0200

    added temporary project parserStart

---
+++ TIC.ParserStart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TIC.ParserStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
