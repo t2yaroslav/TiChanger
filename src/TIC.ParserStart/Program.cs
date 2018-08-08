844093be2fcd23d0c4a26a926b1400e7a946360f
Jacob Kirkwood <jacob.kirkwood@bbconsult.co.uk>
2018-08-08 23:47:40 +0200

    deleted coments

--- src/TIC.ParserStart/Program.cs
+++ src/TIC.ParserStart/Program.cs
@@ -9,21 +9,6 @@ namespace TIC.ParserStart
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Hello!");
-            var parser = new Parser.Parser();
-            var list = parser.Start().Result;
-
-            Console.WriteLine("");
-            Console.WriteLine("Get list of wiki:");
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.ReadKey();
-            */
-
             var restClient = new Rest();
 
             var filter = new ExchangersListFilter()
@@ -33,15 +18,6 @@ namespace TIC.ParserStart
             };
 
             var exchangers = restClient.GetExchangerList(filter);
-            /*
-            Console.WriteLine("REST client!");
-            var restClient = new Rest();
-            var repo = restClient.Go().Result;
-            
-            Console.WriteLine("");
-            Console.WriteLine(repo.Collection.ItemsCount);
-            Console.ReadKey();
-            */
         }
     }
 }
